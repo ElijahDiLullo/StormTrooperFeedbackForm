12c0819eaca27d43f748540bd8f3c5f024915a42
ElijahDiLullo <142916609+ElijahDiLullo@users.noreply.github.com>
2025-03-20 12:09:27 -0400

    did javascript

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.getElementById('stormTrooperForm').addEventListener('submit',function(e){
+document.getElementById('sTF').addEventListener('submit',function(e){
     e.preventDefault();//Prevents the form from submitting immediately.
-    alert("Hi")
-
+    alert("Submit button pressed")
+    let EmailPattern = "/\@/g"
     const firstName = document.getElementById("firstName").value;
     const lastName = document.getElementById("lastName").value;
-    const age = document.getElementById("age").value;
+    const age = parseInt(document.getElementById("age")).value;
     const phone = document.getElementById("phone").value;
     const street = document.getElementById("street").value;
     const city = document.getElementById("city").value;
@@ -12,12 +12,15 @@ document.getElementById('stormTrooperForm').addEventListener('submit',function(e
     const zipcode = document.getElementById("zipcode").value;
     const email = document.getElementById("email").value;
     const feedbackMessage = document.getElementById("feedbackMessage").value;
-    alert(firstName +" "+ lastName + ",email: " +email)
-
+    alert(firstName +" "+ lastName + ",email: ")
+    email.search(EmailPattern)
 
     if (firstName === '' || lastName === '' || age ==='' || phone === '' || email === ''|| street ===''||
-         city ==='' || state ==='' || zipcode ==='' || feedbackMessage ===''  ){
+         city ==='' || state ==='' || zipcode ==='' || feedbackMessage ===''){
         alert("Please complete all the fields.")
+    }
+    else if(email.search(EmailPattern) !== true){
+        alert("Please enter a valid Email!")
     }
     else{
         alert("Form submitted.")
